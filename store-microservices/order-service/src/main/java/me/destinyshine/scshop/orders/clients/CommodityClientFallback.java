57a374dca202c5ef2a5c48eed8106028e1260bcb
destinyliu <destinyliu@yeah.net>
2016-12-22 22:52:07 +0800

    ..

--- store-microservices/order-service/src/main/java/me/destinyshine/scshop/orders/clients/CommodityClientFallback.java
+++ store-microservices/order-service/src/main/java/me/destinyshine/scshop/orders/clients/CommodityClientFallback.java
@@ -1,10 +1,24 @@
 package me.destinyshine.scshop.orders.clients;
 
+import me.destinyshine.scshop.commodity.interfaces.dto.CommodityDTO;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
 
 /**
  * @author destinyliu
  */
 @Component
 public class CommodityClientFallback implements CommodityClient {
+
+  @Override
+  public List<CommodityDTO> getRecommendedCommodities() {
+    return null;
+  }
+
+  @Override
+  public List<CommodityDTO> getCommodities(@RequestHeader("X-Last-Id") String lastId) {
+    return null;
+  }
 }
